73f34628fca92ec25eb97e11c9933003ea05f18a
zakke-username <175124121+zakke-username@users.noreply.github.com>
2024-09-04 18:48:37 +0300

    palautus

--- moduuli-5/3-alkuluvut.py
+++ moduuli-5/3-alkuluvut.py
@@ -1,14 +1,14 @@
-number = int(input("Anna numero: "))
+n = int(input("Anna numero: "))
 prime = True
 
-if number <= 1:
+if n <= 1:
     prime = False
 else:
-    for i in range(2, number):
-        if number % i == 0:
+    for i in range(2, n):
+        if n % i == 0:
             prime = False
 
 if prime:
-    print(f"{number} on alkuluku")
+    print(f"{n} on alkuluku")
 else:
-    print(f"{number} ei ole alkuluku")
+    print(f"{n} ei ole alkuluku")
