50db16304822d682330f7ff115c27b864dd49b01
Zakke <zakkezweyg@gmail.com>
2024-09-12 09:43:36 +0300

    palautus

--- moduuli-6/5-parilliset.py
+++ moduuli-6/5-parilliset.py
@@ -1,6 +1,7 @@
 def main():
-    test_list = [0,1,2,3,4,5,6,7,8,9,10]
-    print(even_numbers(test_list))
+    test_list = [1,2,3,4,5,6,7,8,9,10]
+    print(f"Alkuperäinen lista: {test_list}")
+    print(f"Parittomat luvut: {even_numbers(test_list)}")
 
 def even_numbers(input_list):
     out = []
